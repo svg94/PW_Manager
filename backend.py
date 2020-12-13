3c87e076f5618a7999f21a0733a6c7eeafe11d35
svg94 <liga-2000@hotmail.de>
2020-12-13 19:58:28 +0100

    integrated MongoDB and created first tests

--- backend.py
+++ backend.py
@@ -2,7 +2,14 @@ from flask import Flask, request, render_template
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import sys
+from pymongo import MongoClient
+import json
+
 reader = SimpleMFRC522()
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/DBPasswords"
+DOMAIN = "192.168.100.40"
+MONGO_PORT = 27017
 
 def readPW():
 	text = ""
@@ -11,14 +18,7 @@ def readPW():
 	finally:
 		GPIO.cleanup()
 	return text
-#	text = ""
-#	try:
-#		id, text = reader.read()
-#	finally:
-#		GPIO.cleanup()
-#	return text
 
-app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -30,9 +30,20 @@ def auth():
 	output = readPW()
 	if("Hi" in output):
 		print("Access")
-		return "Access granted."
+		return db()
 	else :
 		print("No")
 		return  "Access denied."
+#@app.route('/db', methods=['GET'])
+def db():
+	documents ={}
+	client = MongoClient()
+	db = client.DBPasswords
+	pws = db.passwords
+	result = pws.find_one()
+	return "Account bei: "+result["name"]+" <br>password: "+result["pw"]
+
+	
+
 
 
